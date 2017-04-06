abc3e046e27c9761904d2a214988eaa242b3db7d
zhangyage <zhangyage2015@163.com>
2017-04-06 17:48:31 +0800

    python-study

--- day03/pickle_json.py
+++ day03/pickle_json.py
@@ -7,6 +7,10 @@
 '''
 import pickle
 li = ['zhang','pan','liu','guan','li']
-b =  pickle.dumps(li)        #序列化字符串
+b =  pickle.dumps(li)        #dumps序列化字符串
  
-print pickle.loads(b)        #反序列化输出
+print pickle.loads(b)        #loads反序列化输出
+
+
+pickle.dump(li,open('temp.pk','w'))         #dump序列化到文件中
+print pickle.load(open('temp.pk','r'))      #load反序列化读取文件
