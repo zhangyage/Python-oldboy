f9d70a41a7a517b27149f5cf2faa68d0744dbfc4
zhangyage <zhangyage2015@163.com>
2017-10-20 10:38:53 +0800

    try-except-else-finally

--- day04/exceptions/TEF.py
+++ day04/exceptions/TEF.py
@@ -3,7 +3,8 @@
 
 #多异常捕获
 try:
-    f = open("2.txt")
+    #f = open("2.txt")
+    f = open("1.txt")
     line = f.read(2)
     num = int(line)
     print "read num=%d" % num
@@ -11,6 +12,8 @@ except IOError,e:   #捕获没有文件的错误
     print "cathe IOError:",e
 except ValueError,e: #捕获文件中的文件必须是数字
     print "cathe ValueError:",e
+else:
+    print "NO Error!"
 finally:        #程序正常执行的时候才执行这一句
     try:
         print "close file!"
