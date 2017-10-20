e71a7ab635a21c9ed4dd070d100d9c3878c025f0
zhangyage <zhangyage2015@163.com>
2017-10-20 15:50:18 +0800

    折线图 散点图 加色折线图

--- Matplotlib/test.py
+++ Matplotlib/test.py
@@ -4,7 +4,17 @@
 import matplotlib.pyplot as plt
 
 year = [1911,1912,1913,1914]
-pop = [1.7,2.5,3.1,3.9]
+pop = [1.7,3,7,10]
+
 #plt.plot(year,pop)    #画折线图
-plt.scatter(year,pop)  #画散点图
+#plt.scatter(year,pop)  #画散点图  
+plt.fill_between(year, pop, 0, color='green')   #人性化显示 有绿色图块
+
+
+plt.xlabel('Year')       #X轴的标签
+plt.ylabel('Population')
+plt.title("world Population Projection")   #标题
+plt.yticks([0,1,2,3,4,5,6,7,8,9,10,11])  #Y轴的刻度
+
+
 plt.show()
