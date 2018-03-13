9c18103ef0cd0979155c08f980e6cd9a19c4cc2c
zhangyage <zhangyage2015@163.com>
2018-03-13 17:29:52 +0800

    致远信息爬取

--- my_study/re_study/xpath_study/wx_num.py
+++ my_study/re_study/xpath_study/wx_num.py
@@ -18,8 +18,19 @@ html = getsource(urls)
 Selector = etree.HTML(html.text)
 content_field = Selector.xpath('//*[@id="js_content"]/table/tbody/tr[@height="20"]')
 item = {}
+total = []
 for each in content_field[1:]:
     project = each.xpath('td/span/text()')
+#     print project[2]
     for i in project:
         print i
+#     item['product'] = project[0]
+#     item['start_time'] = project[1]
+#     item['history_num'] = project[2]
+#     item['new_nuw'] = project[3]
+#     total.append(item)
+# 
+# print total     
+# for i in total:
+#     print i
     
